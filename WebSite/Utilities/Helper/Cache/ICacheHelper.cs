ca4f952a82de6d7c6fd79662ff000ccc708587fe
gqy117 <gqy117@gmail.com>
2014-05-17 12:43:18 +0800

    [master] Refactor SessionHelper as interface.

---
+++ WebSite/Utilities/Helper/Cache/ICacheHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebToolService
+{
+    public interface ICacheHelper
+    {
+        T GetCache<T>(string key, Func<T> func) where T : class;
+    }
+}
