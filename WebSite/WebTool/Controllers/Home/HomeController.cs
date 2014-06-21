30e03b4849d71bb0d0009a1ed4a2f92da3801443
gqy117 <gqy117@gmail.com>
2014-06-21 14:27:51 +0800

    [master] Use reflection to generate localization string.

--- WebSite/WebTool/Controllers/Home/HomeController.cs
+++ WebSite/WebTool/Controllers/Home/HomeController.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Resources;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using WebTool;
+using WebToolCulture.Resource;
 using WebToolService;
 
 namespace WebTool
