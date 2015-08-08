fa7c021b8d4970622b0adc76e1bc19e5f73f9fc3
Leo Li <gqy117@gmail.com>
2015-08-08 10:31:17 +0800

    [JsRefactor] Added register.js.
    Former-commit-id: b80c814811c601c2557fd4b58b6f0c83ae10de71 [formerly b80c814811c601c2557fd4b58b6f0c83ae10de71 [formerly b80c814811c601c2557fd4b58b6f0c83ae10de71 [formerly b80c814811c601c2557fd4b58b6f0c83ae10de71 [formerly afa4789c48f9adf915297046a965c89fdf0654ea]]]]
    Former-commit-id: 3b0aa9384b3d5731a08002663689d3cb5b01c3df
    Former-commit-id: 72abe234a22a6d292092152d26e26aa85c82edf5
    Former-commit-id: 589ea6522dc77e6c936a1e75550244174793b357
    Former-commit-id: 080e0fb6a83eec87ce5d66ac65aaab5458f13479

--- WebSite/WebTool/Views/Head/head.js
+++ WebSite/WebTool/Views/Head/head.js
@@ -10,3 +10,23 @@ function LogOut_Click() {
 function brand_Click() {
     Track('IndexHead', 'brand');
 }
+
+
+function _indexLayout_On_Ready() {
+    jQuery(document).ready(function () {
+        App.init(); // init the rest of plugins and elements
+        UIModals.init();
+    });
+}
+Add_JS_Content('_indexLayout_On_Ready();');
+
+
+function LeftPanel_Dashboard_Click() {
+    Track('Index', 'LeftPanel_Dashboard');
+    window.location.href = App.baseUrl + 'Home/Index';
+
+}
+function LeftPanel_WOL_Click() {
+    Track('Index', 'LeftPanel_WOL');
+    window.location.href = App.baseUrl + 'Tool/WOL';
+}
