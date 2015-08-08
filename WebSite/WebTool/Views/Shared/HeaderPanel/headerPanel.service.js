61405f9888f48d121e2f58ee243a74cd83914a3f
Leo Li <gqy117@gmail.com>
2015-08-08 15:48:03 +0800

    [AngularJsRefactor] Addde HeaderPanel.

---
+++ WebSite/WebTool/Views/Shared/HeaderPanel/headerPanel.service.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    angular
+    .module('mainApp')
+    .factory('headerPanelService', ['wolService', headerPanelService]);
+
+    function headerPanelService(wolService) {
+        return {
+            navigation1Click: function _navigation1Click() {
+                wolService.wol();
+            }
+        };
+    }
+}());
