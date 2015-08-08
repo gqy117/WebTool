2908deb76cdbf1689974424e4fecfa1fdda9663c
Leo Li <gqy117@gmail.com>
2015-08-08 10:55:34 +0800

    [JsRefactor] Removed unused code.

---
+++ WebSite/WebTool/Views/Shared/JS/BottomJS.js
@@ -0,0 +1,9 @@
+﻿// Check for browser support of event handling capability
+if (window.addEventListener) {
+    window.addEventListener("load", add_JS_At_Onload, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent("onload", add_JS_At_Onload);
+} else {
+    window.onload = add_JS_At_Onload;
+}
