13dd4d7d44da66dbe553126d8fffebcb3095b796
Leo Li <gqy117@gmail.com>
2015-08-08 14:57:37 +0800

    [AngularJsRefactor] Added sidebar related js files.
    Former-commit-id: bb970222c21e20d101e6d520ef5765fe86810d70 [formerly bb970222c21e20d101e6d520ef5765fe86810d70 [formerly bb970222c21e20d101e6d520ef5765fe86810d70 [formerly bb970222c21e20d101e6d520ef5765fe86810d70 [formerly b7ca482c049878aab2ea6a60d421439e1402843f]]]]
    Former-commit-id: 30fe31c5874089fa8c7acfd5f0b53f3acf570426
    Former-commit-id: c56b4cb39365fde6f1d977cd84e2c4a7634224c1
    Former-commit-id: 64b84ba0236bb11e2402c7acd6f8bedd60e9d6af
    Former-commit-id: 237b37bc41a9f5dd0148419e0122fadc5d0bbe9a

---
+++ WebSite/WebTool/Views/Sidebar/sidebar.service.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    angular
+    .module('mainApp')
+    .factory('sidebarService', sidebarService);
+
+    function sidebarService() {
+        return {
+            LeftPanel_Dashboard: function _LeftPanel_Dashboard() {
+                Track('Index', 'LeftPanel_Dashboard');
+                window.location.href = App.baseUrl + 'Home/Index';
+            },
+
+            LeftPanel_WOL: function _LeftPanel_WOL() {
+                Track('Index', 'LeftPanel_WOL');
+                window.location.href = App.baseUrl + 'Tool/WOL';
+            }
+        };
+    }
+}());
