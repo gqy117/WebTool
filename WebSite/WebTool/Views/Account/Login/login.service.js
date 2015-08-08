a6350d8ae9067d4e7c938a9daf58099003b8f274
Leo Li <gqy117@gmail.com>
2015-08-08 17:51:47 +0800

    [AngularJsRefactor] Refactored Login.cshtml.

---
+++ WebSite/WebTool/Views/Account/Login/login.service.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    angular
+        .module('mainApp')
+        .factory('loginService', loginService);
+
+    function loginService() {
+        return {
+
+            formLogin_Submit: function _formLogin_Submit() {
+                Track('Login', 'Login');
+                $('#FormLogin').submit();
+            },
+
+            formLogin_Password_keyup: function _formLogin_Password_keyup($event) {
+                if ($event.keyCode === 13) {
+                    this.formLogin_Submit();
+                }
+            },
+
+            formLogin_forgetpassword_Click: function _formLogin_forgetpassword_Click() {
+                Track('Login', 'SignUpNow');
+                window.location.href = App.baseUrl + 'Register/Index';
+            }
+        }
+    }
+}());
