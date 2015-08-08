5556dc473c1881678428448e0d891387b9ebc005
Leo Li <gqy117@gmail.com>
2015-08-08 16:56:21 +0800

    [AngularJsRefactor] Added files for WakeUpPanel.cshtml.

---
+++ WebSite/WebTool/Views/Home/Panel/wakeUpPanel.service.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    angular
+        .module('mainApp')
+        .factory('wakeUpPanelService', wakeUpPanelService);
+
+
+    function wakeUpPanelService() {
+        return {
+
+            wakeUp: function _panel3_ViewMore() {
+                Track('Index', 'Panel3_ViewMore');
+
+                Post(
+                    {
+                        url: App.baseUrl + 'Tool/WakeUp',
+                        //data: JSON.stringify(WOL),
+                        success: function (message) {
+                            $('#alertWakeUpSuccess').show();
+                        }
+                    }
+                );
+            }
+        }
+    }
+}());
