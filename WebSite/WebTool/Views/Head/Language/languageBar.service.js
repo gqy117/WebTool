d2783046e513f5b77e4d4f169a834cc5d14168a0
Leo Li <gqy117@gmail.com>
2015-08-08 15:06:12 +0800

    [AngularJsRefactor] Changed "Language Bar" related js files.
    Former-commit-id: 1e6f8e251fa0bce5ef2a77a2eb0bcf3971bf418b [formerly 1e6f8e251fa0bce5ef2a77a2eb0bcf3971bf418b [formerly 1e6f8e251fa0bce5ef2a77a2eb0bcf3971bf418b [formerly 1e6f8e251fa0bce5ef2a77a2eb0bcf3971bf418b [formerly 8c3536ae5303f1a6140120b11f39792b3b43aa8b]]]]
    Former-commit-id: 3cad9cb17ebb5a8f4666bc79f3d3550f89c2a79a
    Former-commit-id: 23badd71aaced9f765b977f5502195ff6d5e8ef5
    Former-commit-id: fa7f701d2e04b3a510c5c745c71d8e120a6ba312
    Former-commit-id: d531bf56e8923126732e95a64213bb07ad232f80

---
+++ WebSite/WebTool/Views/Head/Language/languageBar.service.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    angular
+    .module('mainApp')
+    .factory('languageBarService', languageBarService);
+
+    function languageBarService() {
+        return {
+            changeLanguage: function _changeLanguage(languageCode) {
+                Track('IndexHead', 'ChangeLanguage');
+                $.cookie("WebToolLanguage", languageCode, {
+                    expires: 10000,
+                    path: '/'
+                });
+                location.reload();
+            }
+        };
+    }
+}());
