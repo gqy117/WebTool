d27d69f113470b5e43d83d25e28e7427727fdb4c
Leo Li <gqy117@gmail.com>
2015-08-09 14:15:11 +0800

    [AngularJsRefactor] Used the angularjs way to active class.

--- WebSite/WebTool/Views/Shared/JS/BottomJS.js
+++
@@ -1,9 +0,0 @@
-﻿// Check for browser support of event handling capability
-if (window.addEventListener) {
-    window.addEventListener("load", add_JS_At_Onload, false);
-}
-else if (window.attachEvent) {
-    window.attachEvent("onload", add_JS_At_Onload);
-} else {
-    window.onload = add_JS_At_Onload;
-}
---
+++ WebSite/WebTool/Views/WOL/wol.controller.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    angular.module("mainApp")
+         .controller('wolController', ['wolService', 'sidebarService', wolController]);
+
+    function wolController(wolService, sidebarService) {
+
+        this.activePanel = sidebarService.activePanel;
+        this.activePanel.wol = true;
+    }
+}());
+
+CreateTable("#WOLTable", App.baseUrl + 'Tool/WOLTable');
