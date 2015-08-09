f51ee55ab299cc2517fba75f73c462483f98d41a
Leo Li <gqy117@gmail.com>
2015-08-09 10:34:03 +0800

    [AngularJsRefactor] Refactored the Bundle files.

---
+++ WebSite/WebTool/App_Start/Bundle/Shared/Bottom.cs
@@ -0,0 +1,16 @@
+﻿namespace WebTool.Bundle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class Bottom
+    {
+        public static readonly string[] Files =
+        {
+            "~/Views/Shared/JS/BottomJS.js",
+            "~/Views/Shared/JS/PageEnd.js"
+        };
+    }
+}
