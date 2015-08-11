70b255ca97b3bfdfd6d2fd7e37b253efbc9a349b
Leo Li <gqy117@gmail.com>
2015-08-11 20:06:00 +0800

    [AngularJsRefactor] Used immediately executed function expressions.

--- WebSite/WebTool/Views/Head/head.controller.js
+++ WebSite/WebTool/Views/Head/head.controller.js
@@ -10,14 +10,11 @@
         this.myProfile_Click = headService.myProfile;
         this.logOut_Click = headService.logOut;
     }
-}());
 
 
-function _indexLayout_On_Ready() {
     jQuery(document).ready(function () {
         App.init(); // init the rest of plugins and elements
         UIModals.init();
     });
-}
-_indexLayout_On_Ready();
 
+}());
