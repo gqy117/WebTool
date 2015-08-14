3639f9c4f6566942f5506f1c3a2d60710d0ccbaa
Leo Li <gqy117@gmail.com>
2015-08-14 20:41:09 +0800

    [JsLint] Removed unnecessary minimization of css files.
    Former-commit-id: 06fb503a80abaeb139e5c0d711ec79a6807a1605 [formerly 06fb503a80abaeb139e5c0d711ec79a6807a1605 [formerly 06fb503a80abaeb139e5c0d711ec79a6807a1605 [formerly 06fb503a80abaeb139e5c0d711ec79a6807a1605 [formerly 071789fe52a707486cb47fd3fd335014b100ca08]]]]
    Former-commit-id: dcbe7a1135197c97d96af54f0a1d5f7372ad670c
    Former-commit-id: 9fe1ca78417a6c1bd1db93c2a59e5e2ac2a4f3cc
    Former-commit-id: 254fa9e70d15addf5453d0dab9e679bede315c62
    Former-commit-id: 10e570a19883240e424cadc6bb360515690025d4

--- WebSite/PreBuild.bat
+++ WebSite/PreBuild.bat
@@ -1,11 +1,3 @@
 SET currentLocation=%~dp0
-SET "projectLocation=%currentLocation%WebTool\"
-SET "ajaxmin=%currentLocation%Dlls\MicrosoftAjaxMinifier\ajaxmin.exe"
-
-%ajaxmin% "%projectLocation%Content\assets\css\style_default.css" -out "%projectLocation%Content\assets\css\style_default.min.css" -clobber
-%ajaxmin% "%projectLocation%Content\assets\uniform\css\uniform.default.css" -out "%projectLocation%Content\assets\uniform\css\uniform.default.min.css" -clobber
-%ajaxmin% "%projectLocation%Content\Site.css" -out "%projectLocation%Content\Site.min.css" -clobber
-%ajaxmin% "%projectLocation%Content\assets\css\style_responsive.css" -out "%projectLocation%Content\assets\css\style_responsive.min.css" -clobber
-%ajaxmin% "%projectLocation%Content\assets\font-awesome\css\font-awesome.css" -out "%projectLocation%Content\assets\font-awesome\css\font-awesome.min.css" -clobber
 
 jshint "%currentLocation%WebTool"
