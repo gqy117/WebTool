7c46f6292f7d7c74024dc87396dc4e99938a15e0
Leo Li <gqy117@gmail.com>
2015-08-16 16:17:57 +0800

    [Specflow] Added a partial class.

---
+++ WebSite/Test/BDD/Base/AlreadyLogOn.cs
@@ -0,0 +1,53 @@
+﻿namespace BDD.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Specflow.Common;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    [Scope(Tag = "AlreadyLogOn")]
+    public class AlreadyLogOn : StepsBase
+    {
+        [When(@"I already LogOn")]
+        public void WhenIAlreadyLogOn()
+        {
+            this.GivenUserNameAndPassword();
+
+            this.GotoLogOnPage();
+
+            this.FillUserNameAndPassword();
+
+            this.StartLogOn();
+        }
+
+        private void StartLogOn()
+        {
+            this.WhenIClickSubmitById("login-btn");
+        }
+
+        private void FillUserNameAndPassword()
+        {
+            Table tableElementName = new Table(new string[] { "UserName", "Password" });
+            tableElementName.AddRow(new string[] { "UserName", "Password" });
+
+            this.WhenIFillFollowingElementsByName(tableElementName);
+        }
+
+        private void GotoLogOnPage()
+        {
+            this.WhenIOpenPage("~/Account/Login");
+        }
+
+        private void GivenUserNameAndPassword()
+        {
+            Table tableElementValue = new Table(new string[] { "UserName", "Password" });
+            tableElementValue.AddRow(new string[] { "1", "1" });
+
+            this.GivenInformation(tableElementValue);
+        }
+    }
+}
