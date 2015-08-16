b0cff43e3f001bfbccfc69bbd0b4821eccbc3824
Leo Li <gqy117@gmail.com>
2015-08-16 08:55:34 +0800

    [Specflow] Added Specflow.Common to the solution.

---
+++ WebSite/Specflow.Common/BaseClass/TestBase.cs
@@ -0,0 +1,91 @@
+﻿namespace Specflow.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Remote;
+    using TechTalk.SpecFlow;
+
+    public abstract class TestBase
+    {
+        #region Properties
+
+        public virtual RemoteWebDriver Browser
+        {
+            get;
+            set;
+        }
+
+        public TableRow UserInfo { get; set; }
+
+        protected virtual Encoding DefaultEncoding
+        {
+            get { return Encoding.UTF8; }
+        }
+        #endregion
+
+        [AfterScenario]
+        public virtual void TearDown()
+        {
+            this.Browser.Quit();
+        }
+
+        protected virtual void FillTheFormByName(Table tableKey, TableRow rowValue)
+        {
+            var firstRow = tableKey.Rows.First();
+
+            for (int i = 0; i < firstRow.Keys.Count; i++)
+            {
+                this.Browser.FindElementByName(firstRow.Values.ElementAt(i)).SendKeys(rowValue[i]);
+            }
+        }
+
+        protected virtual void FillTheFormById(Table tableKey, TableRow rowValue)
+        {
+            var firstRow = tableKey.Rows.First();
+
+            for (int i = 0; i < firstRow.Keys.Count; i++)
+            {
+                this.Browser.FindElementById(firstRow.Values.ElementAt(i)).SendKeys(rowValue[i]);
+            }
+        }
+
+        protected void OpenPage(string url)
+        {
+            this.Browser.Navigate().GoToUrl(url);
+        }
+
+        protected void ClickById(string buttonId)
+        {
+            this.Browser.FindElementById(buttonId).Click();
+        }
+
+        protected void ClickByName(string buttonName)
+        {
+            this.Browser.FindElementByName(buttonName).Click();
+        }
+
+        protected virtual string ReadFileString(string filePath)
+        {
+            string fileString;
+
+            using (StreamReader sr = new StreamReader(filePath, this.DefaultEncoding))
+            {
+                fileString = sr.ReadToEnd();
+            }
+
+            return fileString;
+        }
+
+        protected string RemoveWhiteSpace(string actual)
+        {
+            return Regex.Replace(actual, @"\s+", string.Empty);
+        }
+    }
+}
