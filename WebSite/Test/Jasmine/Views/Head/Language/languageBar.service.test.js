d10ea5110a63bb115861e5b2e032cc0bfa0c8412
Leo Li <gqy117@gmail.com>
2015-08-22 18:26:23 +0800

    [Jasmine] Added Jasmine to the project.
    Former-commit-id: 14a32b7be97e253de7da803e2d52898f3eba0296 [formerly 14a32b7be97e253de7da803e2d52898f3eba0296 [formerly 14a32b7be97e253de7da803e2d52898f3eba0296 [formerly 14a32b7be97e253de7da803e2d52898f3eba0296 [formerly 3318262e04cc69406ca13d60d38ad2379a25aa4b]]]]
    Former-commit-id: 052da1c9223daeb488b4122ec7eb5add48f5e224
    Former-commit-id: 50a31e6a0eeeb9a5cbda4036a653e0493527cc98
    Former-commit-id: f19055a3b158f950b6610a58e8354ba102dca071
    Former-commit-id: be88e4413fd1ba41cbcb78bcc97ce52d7a44acb2

---
+++ WebSite/Test/Jasmine/Views/Head/Language/languageBar.service.test.js
@@ -0,0 +1,13 @@
+﻿describe("languageBar.service.test", function () {
+
+    sharedSetup();
+
+    it("changeLanguage should set cookie to 'en'", inject(function (languageBarService) {
+        var controller = languageBarService;
+
+        controller.changeLanguage('en');
+
+        expect($window.Track).toHaveBeenCalled();
+        expect($window.location.reload).toHaveBeenCalled();
+    }));
+});
