890ff3ea2d1cd7b1a05c1f2d53af8fd4e1c2df68
Leo Li <gqy117@gmail.com>
2015-08-22 18:26:23 +0800

    [Jasmine] Added Jasmine to the project.

---
+++ WebSite/Test/Jasmine/beforeEachAfterEach.js
@@ -0,0 +1,30 @@
+﻿var $window;
+
+function sharedSetup() {
+
+    // mainApp
+    beforeEach(module('mainApp'));
+
+    // before
+    beforeEach(function () {
+        module(mockUpWindow);
+    });
+
+    // after
+    afterEach(function () {
+    });
+
+
+
+    // window
+    function mockUpWindow($provide) {
+        $window = {
+            Track: jasmine.createSpy('Track'),
+            location: {
+                reload: jasmine.createSpy('reload')
+            }
+        };
+
+        $provide.value('$window', $window);
+    }
+};
