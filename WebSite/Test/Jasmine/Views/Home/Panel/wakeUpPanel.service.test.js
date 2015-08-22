67b132e211fb4e548e0c0a3f6d217a3d0e9c7262
Leo Li <gqy117@gmail.com>
2015-08-22 21:04:13 +0800

    [Jasmine] Added "wakeUpPanel.service.test.js".
    Former-commit-id: 562b2597f7bf2dec70ff89a96b10104634ac920e [formerly 562b2597f7bf2dec70ff89a96b10104634ac920e [formerly 562b2597f7bf2dec70ff89a96b10104634ac920e [formerly 562b2597f7bf2dec70ff89a96b10104634ac920e [formerly 4fa9c3403de1d5c84dc2a29b2f4863e610ede389]]]]
    Former-commit-id: b304816ea2fbdad0f54214efdbd79b893fba35fc
    Former-commit-id: 11307b8ac44d0204603afae0946c07abc309ef49
    Former-commit-id: 78121dc3e3ed00ca913d3171a77e2e3b6984e937
    Former-commit-id: 5510cc04f17ea8842da981d32d78f927d395ac3c

---
+++ WebSite/Test/Jasmine/Views/Home/Panel/wakeUpPanel.service.test.js
@@ -0,0 +1,13 @@
+﻿describe("wakeUpPanel.service.test", function () {
+
+    sharedSetup();
+
+    it("hideMessage should set isShowAlertWakeUpSuccess to false", inject(function (wakeUpPanelService) {
+        var service = wakeUpPanelService;
+
+        service.hideMessage();
+
+        expect(service.isShowAlertWakeUpSuccess).toEqual(false);
+    }));
+
+});
