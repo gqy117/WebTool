e325112c7f32be4bbfd89adfe5779363b34adbbf
Leo Li <gqy117@gmail.com>
2015-08-23 15:43:42 +0800

    [Jasmine] Mock $httpBackend.
    Former-commit-id: 45764843507ad5d8caee6501de1d519cdbee7033 [formerly 45764843507ad5d8caee6501de1d519cdbee7033 [formerly 45764843507ad5d8caee6501de1d519cdbee7033 [formerly 45764843507ad5d8caee6501de1d519cdbee7033 [formerly 2e9b4991bd8928289c42c69c0823d3655eedb0ad]]]]
    Former-commit-id: 99fe638277eee8cf31593d08d69640739ecd3c80
    Former-commit-id: 68c7c806f3866ca84e63f31273e5160784474564
    Former-commit-id: 343509995854989ec9b03b5a789c42757795814d
    Former-commit-id: 41304fc2ec10197db1cb241192bdfdec78a107fc

--- WebSite/Test/Jasmine/Views/Head/Language/languageBar.service.test.js
+++ WebSite/Test/Jasmine/Views/Head/Language/languageBar.service.test.js
@@ -1,14 +1,16 @@
 ﻿describe("languageBar.service.test", function () {
+    var service;
 
-    sharedSetup();
+    beforeEach(inject(function (languageBarService) {
+        service = languageBarService;
+    }));
 
-    it("changeLanguage() should set cookie to 'en'", inject(function (languageBarService) {
-        var service = languageBarService;
-        
+
+    it("changeLanguage() should set cookie to 'en'", function () {
         service.changeLanguage('en');
         
         expect($window.Track).toHaveBeenCalled();
         expect($window.jQuery.cookie).toHaveBeenCalled();
         expect($window.location.reload).toHaveBeenCalled();
-    }));
+    });
 });
--- WebSite/Test/Jasmine/Views/Sidebar/sideBar.service.test.js
+++ WebSite/Test/Jasmine/Views/Sidebar/sideBar.service.test.js
@@ -1,22 +1,23 @@
 ﻿describe("sidebar.service.test", function () {
+    var service;
 
-    sharedSetup();
+    // setup
+    beforeEach(inject(function (sidebarService) {
+        service = sidebarService;
+    }));
 
-    it("LeftPanel_Dashboard should set href to '/Home/Index'", inject(function (sidebarService) {
-        var service = sidebarService;
 
+    it("LeftPanel_Dashboard should set href to '/Home/Index'", function () {
         service.LeftPanel_Dashboard();
 
         expect($window.Track).toHaveBeenCalled();
         expect($window.location.href).toEqual('/Home/Index');
-    }));
+    });
 
-    it("LeftPanel_WOL should set href to '/Tool/WOL'", inject(function (sidebarService) {
-        var service = sidebarService;
-
+    it("LeftPanel_WOL should set href to '/Tool/WOL'", function () {
         service.LeftPanel_WOL();
 
         expect($window.Track).toHaveBeenCalled();
         expect($window.location.href).toEqual('/Tool/WOL');
-    }));
+    });
 });
