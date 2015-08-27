45e722469e3be31538f5718cd81328332f4eceac
Leo Li <gqy117@gmail.com>
2015-08-27 21:22:55 +0800

    [TypeScript] Added head.controller.ts.
    Former-commit-id: c55a61c82461681d4686a9f0674b536beeab176e [formerly c55a61c82461681d4686a9f0674b536beeab176e [formerly c55a61c82461681d4686a9f0674b536beeab176e [formerly c55a61c82461681d4686a9f0674b536beeab176e [formerly 83090cd4a7df41c097078dc74ec197a6d07327fe]]]]
    Former-commit-id: bc84fc4016d1cf0597fd032a95976db7235637a5
    Former-commit-id: a17dd3042281111de7d49f62cead0981d8569461
    Former-commit-id: e57a6cc80cc9835add4c0cb24d757dfcc7a72f70
    Former-commit-id: 0c71661ff69bee4ea5c79ed74caa5c401d2787f2

--- WebSite/WebTool/Views/Head/head.controller.js
+++ WebSite/WebTool/Views/Head/head.controller.js
@@ -1,15 +1,30 @@
-﻿(function () {
-    "use strict";
+﻿/// <reference path="head.service.ts" />
+"use strict";
+var M;
+(function (M) {
+    var headController = (function () {
+        // constructor
+        function headController(headService) {
+            this.headService = headService;
+        }
+        // methods
+        headController.prototype.brand_Click = function () {
+            this.headService.brand();
+        };
 
-    angular.module("mainApp")
-        .controller('headController', headController);
+        headController.prototype.myProfile_Click = function () {
+            this.headService.myProfile();
+        };
 
-    headController.$inject = ['headService'];
+        headController.prototype.logOut_Click = function () {
+            this.headService.logOut();
+        };
+        headController.$inject = ["headService"];
+        return headController;
+    })();
+    M.headController = headController;
+})(M || (M = {}));
 
-    function headController(headService) {
-
-        this.brand_Click = headService.brand;
-        this.myProfile_Click = headService.myProfile;
-        this.logOut_Click = headService.logOut;
-    }
-}());
+// init
+angular.module("mainApp").controller("headController", M.headController);
+//# sourceMappingURL=head.controller.js.map
