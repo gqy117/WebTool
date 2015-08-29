1e2d2bba326dd1a3e9c310588ea44638a1925a91
Leo Li <gqy117@gmail.com>
2015-08-29 10:19:15 +0800

    [TypeScript] Added main.controller.ts.

---
+++ WebSite/WebTool/Views/Shared/Init/main.controller.ts
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+module M {
+    export class mainController {
+        // inject
+        static $inject = ["$window", "uiModelService"];
+
+        // constructor
+        constructor(private $window, private uiModelService) {
+            angular.element(document).ready(() => {
+                $window.App.init();
+                uiModelService.init();
+            });
+        }
+    }
+
+    // init
+    angular.module('mainApp')
+        .controller('mainController', mainController);
+}
