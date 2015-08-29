1c5b9e182f3fcd339b1d339e6b02e0a40dee0962
Leo Li <gqy117@gmail.com>
2015-08-29 14:47:51 +0800

    [TypeScript] Added register.service.ts and register.controller.ts.

--- WebSite/WebTool/Views/Account/Register/register.service.js
+++ WebSite/WebTool/Views/Account/Register/register.service.js
@@ -1,24 +1,27 @@
-﻿(function () {
-    "use strict";
-
-    angular
-        .module('mainApp')
-        .factory('registerService', registerService);
-
-    registerService.$inject = ['$window'];
-
-    function registerService($window) {
-        return {
-            submit: function submit() {
-                $window.jQuery('#FormRegister').submit();
-                $window.Track('Register', 'Register');
-            },
-
-            formRegister_Password_keyup: function formRegisterPasswordKeyup($event) {
-                if ($event.keyCode === 13) {
-                    this.submit();
-                }
+"use strict";
+var M;
+(function (M) {
+    var registerService = (function () {
+        // constructor
+        function registerService($window) {
+            this.$window = $window;
+        }
+        // methods
+        registerService.prototype.submit = function () {
+            this.$window.jQuery('#FormRegister').submit();
+            this.$window.Track('Register', 'Register');
+        };
+        registerService.prototype.formRegister_Password_keyup = function ($event) {
+            if ($event.keyCode === 13) {
+                this.submit();
             }
         };
-    }
-}());
+        // inject
+        registerService.$inject = ['$window'];
+        return registerService;
+    })();
+    M.registerService = registerService;
+    // init
+    angular.module('mainApp')
+        .service('registerService', registerService);
+})(M || (M = {}));
