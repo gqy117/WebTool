295175198efc221d8cee9d0877b9d12ae8af01da
Leo Li <gqy117@gmail.com>
2015-08-29 14:13:59 +0800

    [TypeScript] Added login.service.ts.
    Former-commit-id: 17330080abe01d7afc047e9a2fb55bd1ba3a7214 [formerly 17330080abe01d7afc047e9a2fb55bd1ba3a7214 [formerly 17330080abe01d7afc047e9a2fb55bd1ba3a7214 [formerly 17330080abe01d7afc047e9a2fb55bd1ba3a7214 [formerly f20947b29d8fb3be9b3cdd4fa1afedcc3bed9ffa]]]]
    Former-commit-id: ef8549bb13a028661987b1a916af75100874f999
    Former-commit-id: 60ba249c22b85bc1d2d4ca22c7cea269612f6703
    Former-commit-id: 66132977c3f48855b7a125a60fe15d1d8b48a5b7
    Former-commit-id: dd458c3e86d3b2327bed333d27e4634b1e71cea5

---
+++ WebSite/WebTool/Views/Account/LogOn/login.service.ts
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+module M {
+    export class loginService {
+        // inject
+        static $inject = ["$window"];
+
+        // constructor
+        constructor(private $window) { }
+
+        //  methods
+        public formLogOnSubmit(): void {
+            this.$window.Track('LogOn', 'LogOn');
+            this.$window.jQuery('#FormLogin').submit();
+        }
+
+        public formLogOnPasswordKeyup($event): void {
+            if ($event.keyCode === 13) {
+                this.formLogOnSubmit();
+            }
+        }
+
+        public signUpNow(): void{
+            this.$window.Track('LogOn', 'SignUpNow');
+            this.$window.location.href = this.$window.App.baseUrl + 'Register/Index';
+        }
+    }
+
+    // init
+    angular.module('mainApp')
+        .service('loginService', loginService);
+}
