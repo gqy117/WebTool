df64e8e2449aa02cab347e518ed453bf5780e716
Leo Li <gqy117@gmail.com>
2015-08-29 09:50:39 +0800

    [TypeScript] Added uiModel.service.ts.

--- WebSite/WebTool/Views/Shared/Init/uiModel.service.ts
+++ WebSite/WebTool/Views/Shared/Init/uiModel.service.ts
@@ -1,21 +1,19 @@
-"use strict";
-var M;
-(function (M) {
-    var uiModelService = (function () {
-        function uiModelService() {
-        }
+﻿"use strict";
+
+module M {
+    export class uiModelService{
         // methods
-        uiModelService.prototype.init = function () {
+        public init(): void {
             this.initModals();
-        };
-        uiModelService.prototype.initModals = function () {
+        }
+
+        private initModals(): void {
             $.fn.modalmanager.defaults.resize = true;
             $.fn.modalmanager.defaults.spinner = '<div class="loading-spinner fade" style="width: 200px; margin-left: -100px;"><img src="/Content/assets/img/ajax-modal-loading.gif" align="middle">&nbsp;<span style="font-weight:300; color: #eee; font-size: 18px; font-family:Open Sans;">&nbsp;Loading...</span></div>';
-        };
-        return uiModelService;
-    })();
-    M.uiModelService = uiModelService;
+        }
+    }
+
     // init
     angular.module('mainApp')
         .service('uiModelService', uiModelService);
-})(M || (M = {}));
+}
