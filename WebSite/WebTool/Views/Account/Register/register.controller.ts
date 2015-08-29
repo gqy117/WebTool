975dfd451311bfda422d2cf85fdac94164ef2042
Leo Li <gqy117@gmail.com>
2015-08-29 14:47:51 +0800

    [TypeScript] Added register.service.ts and register.controller.ts.
    Former-commit-id: 7a42f9100a2d45411f7ae4b2aa1204e388353f58 [formerly 7a42f9100a2d45411f7ae4b2aa1204e388353f58 [formerly 7a42f9100a2d45411f7ae4b2aa1204e388353f58 [formerly 7a42f9100a2d45411f7ae4b2aa1204e388353f58 [formerly 6385f732b5645895df48a0e2d378879df5438d87]]]]
    Former-commit-id: 2ca55fbd307748bdb86e3dc78d6cc157515b220b
    Former-commit-id: b9926dc15d59710d4775f657deeee5d73176cde5
    Former-commit-id: 02dc720a7a52b85e6decbfe93dfcef6ee6e72f9e
    Former-commit-id: 4b093e665424bb3763b2b5bdbc34dfe5b4682332

---
+++ WebSite/WebTool/Views/Account/Register/register.controller.ts
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+module M {
+    export class registerController {
+        // inject
+        static $inject = ["registerService"];
+
+        // constructor
+        constructor(private registerService: registerService) { }
+
+        // methods
+        public submit(): void {
+            this.registerService.submit();
+        }
+
+        public formRegister_Password_keyup($event): void {
+            this.registerService.formRegister_Password_keyup($event);
+        }
+    }
+
+    // init
+    angular.module("mainApp")
+        .controller('registerController', registerController);
+
+}
