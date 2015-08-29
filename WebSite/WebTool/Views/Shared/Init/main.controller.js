44b9c17b2fd41bbf36ebd5a415bf0ec9581dab2a
Leo Li <gqy117@gmail.com>
2015-08-29 10:19:15 +0800

    [TypeScript] Added main.controller.ts.
    Former-commit-id: 359147d020d8fdf83b441fb8ffe28a559af3fc1e [formerly 359147d020d8fdf83b441fb8ffe28a559af3fc1e [formerly 359147d020d8fdf83b441fb8ffe28a559af3fc1e [formerly 359147d020d8fdf83b441fb8ffe28a559af3fc1e [formerly f49b2460b8e3dc94a17154f15829dd55d7565360]]]]
    Former-commit-id: 945ad46d3ee30e291e9f7bbe755b82c16f9be4ea
    Former-commit-id: 3c030e5fe2a27e9805dc9a6c4cbf7b078e8dea1e
    Former-commit-id: 099c84bc9478593b30ddb01662cc56f7bdbdcca9
    Former-commit-id: 9b56affe9687435d2c0b8425a42f3f79295b3e06

--- WebSite/WebTool/Views/Shared/Init/main.controller.js
+++ WebSite/WebTool/Views/Shared/Init/main.controller.js
@@ -1,18 +1,22 @@
-﻿(function () {
-    "use strict";
-
+"use strict";
+var M;
+(function (M) {
+    var mainController = (function () {
+        // constructor
+        function mainController($window, uiModelService) {
+            this.$window = $window;
+            this.uiModelService = uiModelService;
+            angular.element(document).ready(function () {
+                $window.App.init();
+                uiModelService.init();
+            });
+        }
+        // inject
+        mainController.$inject = ["$window", "uiModelService"];
+        return mainController;
+    })();
+    M.mainController = mainController;
+    // init
     angular.module('mainApp')
         .controller('mainController', mainController);
-
-    mainController.$inject = ['uiModelService'];
-
-    function mainController(uiModelService) {
-
-        angular.element(document).ready(documentReady);
-
-        function documentReady() {
-            App.init();
-            uiModelService.init();
-        }
-    }
-})();
+})(M || (M = {}));
