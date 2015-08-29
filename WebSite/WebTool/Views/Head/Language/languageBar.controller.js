6c8f50a486056c58e4624abb754893e611099bd1
Leo Li <gqy117@gmail.com>
2015-08-29 10:52:44 +0800

    [TypeScript] Added "languagebar.service.ts" and "languageBar.controller.ts".

--- WebSite/WebTool/Views/Head/Language/languageBar.controller.js
+++ WebSite/WebTool/Views/Head/Language/languageBar.controller.js
@@ -1,13 +1,21 @@
-﻿(function () {
-    "use strict";
-
+"use strict";
+var M;
+(function (M) {
+    var languageBarController = (function () {
+        // constructor
+        function languageBarController(languageBarService) {
+            this.languageBarService = languageBarService;
+        }
+        // methods
+        languageBarController.prototype.changeLanguage = function (languageCode) {
+            this.languageBarService.changeLanguage(languageCode);
+        };
+        // inject
+        languageBarController.$inject = ["languageBarService"];
+        return languageBarController;
+    })();
+    M.languageBarController = languageBarController;
+    // init
     angular.module("mainApp")
-         .controller('languageBarController', languageBarController);
-
-    languageBarController.$inject = ['languageBarService'];
-
-    function languageBarController(languageBarService) {
-
-        this.changeLanguage = languageBarService.changeLanguage;
-    }
-}());
+        .controller('languageBarController', languageBarController);
+})(M || (M = {}));
