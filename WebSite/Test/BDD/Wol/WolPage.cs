ec731f06afd2ea1a3bcece7c0a127d70a610d8c1
Leo Li <gqy117@gmail.com>
2016-01-01 11:26:31 +1300

    [SeleniumRefactor] Refactorted "WolPage" page.

---
+++ WebSite/Test/BDD/Wol/WolPage.cs
@@ -0,0 +1,29 @@
+﻿namespace BDD.Wol
+{
+    using System;
+    using Specflow.Common;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class WolPage
+    {
+        private readonly CommonSteps commonSteps;
+
+        public WolPage(CommonSteps commonSteps)
+        {
+            this.commonSteps = commonSteps;
+        }
+
+        [When(@"I open WOL page")]
+        public void WhenIOpenWOLPage()
+        {
+            this.commonSteps.OpenPage("~/Tool/WOL");
+        }
+        
+        [Then(@"the wol table result should be the same as WolTable\.html")]
+        public void ThenTheWolTableResultShouldBeTheSameAsWolTable_Html()
+        {
+            this.commonSteps.ThenTheResultShouldBeTheSameAsTheHtmlFile("#WOLTable tbody tr:nth-child(1)", "Wol\\WolTable.html");
+        }
+    }
+}
