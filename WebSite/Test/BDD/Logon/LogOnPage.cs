9bb458c7a595a3a6e18bc59675b4cd62e5000f4b
Leo Li <gqy117@gmail.com>
2016-01-01 10:44:00 +1300

    [SeleniumRefactor] Refactored "Logon" page.
    Former-commit-id: eb03052d8ac859d4e8d7dc7312b7f81de3c8bb60 [formerly eb03052d8ac859d4e8d7dc7312b7f81de3c8bb60 [formerly eb03052d8ac859d4e8d7dc7312b7f81de3c8bb60 [formerly eb03052d8ac859d4e8d7dc7312b7f81de3c8bb60 [formerly 21f44258f1722b3095b9c1f24d84e21365844ba0]]]]
    Former-commit-id: a9d47b43c398a42ccc93bf47a0702f3ab79f6b4c
    Former-commit-id: 165ee098c0238da2af4ad99c41d343e9b7d6deb3
    Former-commit-id: 4f5ee032fb29d6acb55c930451ff9c6b59605ade
    Former-commit-id: b76b5ff6ad11ef3af3a71774ace43fc90cd63263

---
+++ WebSite/Test/BDD/Logon/LogOnPage.cs
@@ -0,0 +1,49 @@
+﻿namespace BDD.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using OpenQA.Selenium.Remote;
+    using Specflow.Common;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class LogOnToTheWebsite
+    {
+        private readonly CommonSteps commonSteps;
+
+        public LogOnToTheWebsite(CommonSteps commonSteps)
+        {
+            this.commonSteps = commonSteps;
+        }
+
+        [When(@"I goto logon page")]
+        public void WhenIGotoLogonPage()
+        {
+            this.commonSteps.OpenPage("~/Account/Login");
+        }
+
+        [When(@"I fill the username and password")]
+        public void WhenIFillTheUsernameAndPassword()
+        {
+            Table tableElementName = new Table(new string[] { "UserName", "Password" });
+            tableElementName.AddRow(new string[] { "UserName", "Password" });
+
+            this.commonSteps.FillTheFormByName(tableElementName, this.commonSteps.UserInfo);
+        }
+
+        [When(@"I start logon")]
+        public void WhenIStartLogon()
+        {
+            this.commonSteps.ClickById("login-btn");
+        }
+
+        [Then(@"I should see the url is base url")]
+        public void ThenIShouldSeeTheUrlIsBaseUrl()
+        {
+            this.commonSteps.ThenTheCurrentUrlShouldBe("~/");
+        }
+    }
+}
