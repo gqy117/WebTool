00e2d65422108eac97174921bb24e4df15c8ac5d
Leo Li <gqy117@gmail.com>
2016-01-01 20:13:44 +1300

    [SeleniumRefactor] All the dom elements are got by attributes now.

--- WebSite/Test/BDD/Wol/WolPage.cs
+++ WebSite/Test/BDD/Wol/WolPage.cs
@@ -1,6 +1,8 @@
 ﻿namespace BDD.Wol
 {
     using System;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.PageObjects;
     using Specflow.Common;
     using TechTalk.SpecFlow;
 
@@ -9,23 +11,27 @@
     {
         public const string ToolWolUrl = "~/Tool/WOL";
         public const string WolWoltableHtml = "Wol\\WolTable.html";
-        public const string WoltableTbodyTrNthChild = "#WOLTable tbody tr:nth-child(1)";
 
         public WolPage(CommonSteps commonSteps)
             : base(commonSteps)
         {
         }
 
+        #region Properties
+        [FindsBy(How = How.CssSelector, Using = "#WOLTable tbody tr:nth-child(1)")]
+        public IWebElement WoltableTbodyTrNthChild { get; set; }
+        #endregion
+
         [When(@"I open WOL page")]
         public void WhenIOpenWOLPage()
         {
             this.CommonSteps.OpenPage(ToolWolUrl);
         }
-        
+
         [Then(@"the wol table result should be the same as WolTable\.html")]
         public void ThenTheWolTableResultShouldBeTheSameAsWolTable_Html()
         {
-            this.CommonSteps.ThenTheResultShouldBeTheSameAsTheHtmlFile(WoltableTbodyTrNthChild, WolWoltableHtml);
+            this.CommonSteps.ThenTheResultShouldBeTheSameAsTheHtmlFile(this.WoltableTbodyTrNthChild, WolWoltableHtml);
         }
     }
 }
