56b63db4dd11a711474970016809d770594815cc
Leo Li <gqy117@gmail.com>
2016-01-01 13:34:29 +1300

    [SeleniumRefactor] Removed nuget package CsQuery.

--- WebSite/Test/Specflow.Common/BaseClass/WebDriverExtensions.cs
+++ WebSite/Test/Specflow.Common/BaseClass/WebDriverExtensions.cs
@@ -15,8 +15,10 @@
             if (timeoutInSeconds > 0)
             {
                 var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+
                 return wait.Until(drv => drv.FindElement(by));
             }
+
             return driver.FindElement(by);
         }
     }
