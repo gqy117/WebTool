9af37493cf0f6097b69b597f64a02d8b780b7e8d
Leo Li <Leo.Li@myob.com>
2016-01-15 08:00:11 +1300

    Create "Web.config" on the fly.

---
+++ Setup.bat
@@ -0,0 +1,10 @@
+# Create Web.config
+Xcopy WebSite\WebTool\Web.template.config WebSite\WebTool\Web.config 
+
+
+# Install Gulp
+npm install -g gulp
+
+# Install Dependencies
+npm install --prefix WebSite gulp-typescript
+
