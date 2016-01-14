855e734493df7497c0a5e3117fcd5ffc759cd4fd
Leo Li <Leo.Li@myob.com>
2016-01-15 09:23:37 +1300

    Removed redundant lines.
    Former-commit-id: eeab505cc6821ec667570e15bd337553e3370426 [formerly eeab505cc6821ec667570e15bd337553e3370426 [formerly eeab505cc6821ec667570e15bd337553e3370426 [formerly eeab505cc6821ec667570e15bd337553e3370426 [formerly 220c62977911f634b03699090d92c8c8d901fcdd]]]]
    Former-commit-id: e5861e0841f5019f7e95d455fbf557e73080acfb
    Former-commit-id: adedde40ac7f79f0cb5fa1a5da4441a2bb185e93
    Former-commit-id: 1fc70564f84b7421196cdec400d7c91cbd9715f9
    Former-commit-id: 9c9c4d5b604ab3d050f525be181474c7c40ef2fa

--- Setup.bat
+++ Setup.bat
@@ -1,8 +1,4 @@
 REM Create Web.config
 copy WebSite\WebTool\Web.template.config WebSite\WebTool\Web.config 
-
-REM Install Gulp
 npm install -g gulp
-
-REM Install Dependencies
 npm install --prefix WebSite
