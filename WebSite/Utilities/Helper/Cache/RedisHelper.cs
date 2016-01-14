47e236661a259d70d532d70a321f1e93c8d1e915
Leo Li <gqy117@gmail.com>
2016-01-14 19:05:25 +1300

    [Redis] Upgraded to .net framework 4.5. And Implemented redis cache.
    Former-commit-id: 1d46f0f771c5087dbc6bd96d7a168b72caddebb8 [formerly 1d46f0f771c5087dbc6bd96d7a168b72caddebb8 [formerly 1d46f0f771c5087dbc6bd96d7a168b72caddebb8 [formerly 1d46f0f771c5087dbc6bd96d7a168b72caddebb8 [formerly e36cfdfac4e0aa1ffad68638dfa85a115cf82e79]]]]
    Former-commit-id: f227cded9ca1a2b22084733d9e3cb4267ada8e34
    Former-commit-id: 766845726dedb584b71c400793c5e47c7fdd2625
    Former-commit-id: 35269c567a1943b18fc769b9fcde6193aeaf162f
    Former-commit-id: 903c9df311c52dd50ab34512b84616a6618b11fc

---
+++ WebSite/Utilities/Helper/Cache/RedisHelper.cs
@@ -0,0 +1,38 @@
+﻿namespace WebToolService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using StackExchange.Redis.Extensions.Core;
+    using StackExchange.Redis.Extensions.Jil;
+
+    public class RedisHelper : ICacheHelper
+    {
+        private static StackExchangeRedisCacheClient stackExchangeRedisCacheClient = new StackExchangeRedisCacheClient(new JilSerializer());
+
+        public static StackExchangeRedisCacheClient StackExchangeRedisCacheClient
+        {
+            get { return stackExchangeRedisCacheClient; }
+
+            set { stackExchangeRedisCacheClient = value; }
+        }
+
+        public T GetCache<T>(string key, Func<T> func) where T : class
+        {
+            T obj = StackExchangeRedisCacheClient.Exists(key) as T;
+
+            if (StackExchangeRedisCacheClient.Exists(key))
+            {
+                obj = StackExchangeRedisCacheClient.Get<T>(key);
+            }
+            else
+            {
+                obj = func();
+                StackExchangeRedisCacheClient.Add(key, obj);
+            }
+
+            return obj;
+        }
+    }
+}
