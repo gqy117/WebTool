cda70ce4dcda78597d3c20f857e8e0976e262572
Leo Li <gqy117@gmail.com>
2016-01-15 22:38:05 +1300

    [master] Stop restarting iis.
    Former-commit-id: c983207dc25b69c4f131b92513f300e86d8f0641 [formerly c983207dc25b69c4f131b92513f300e86d8f0641 [formerly c983207dc25b69c4f131b92513f300e86d8f0641 [formerly c983207dc25b69c4f131b92513f300e86d8f0641 [formerly 31145609fd71e57ea737eb48fe23086f1a393e2a]]]]
    Former-commit-id: 390b5b94f9e15518ed7816d28973b07e7cce7c8b
    Former-commit-id: 123b49fcdd1a1aee1d95eb9aa491395ced20194c
    Former-commit-id: 3721b6ec53bcf1161bf8290aa79376248f10e483
    Former-commit-id: 36562cec851468a4047d5f15061450c89eb40600

--- CreateIISWebSite.ps1
+++ CreateIISWebSite.ps1
@@ -3,5 +3,3 @@ Set-ItemProperty IIS:\AppPools\WebTool managedRuntimeVersion v4.0
 
 New-Item iis:\Sites\WebTool -bindings @{protocol="http";bindingInformation=":12598:"} -physicalPath "$($env:appveyor_build_folder)\WebSite\WebTool"
 Set-ItemProperty IIS:\Sites\WebTool -name applicationPool -value WebTool
-
-invoke-command -scriptblock {iisreset}
