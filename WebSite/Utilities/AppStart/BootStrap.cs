ca0483cba805e88ecd9840ebac42c76cfbb91eea
Leo Li <gqy117@gmail.com>
2016-01-15 22:22:36 +1300

    [master] Use session helper.

--- WebSite/Utilities/AppStart/BootStrap.cs
+++ WebSite/Utilities/AppStart/BootStrap.cs
@@ -24,23 +24,23 @@
         private static void OnConfigure()
         {
             ////Builder.RegisterType<MemcachedHelper>().As<ICacheHelper>();
-            ////Builder.RegisterInstance(new SessionHelper()).As<ICacheHelper>();
+            Builder.RegisterInstance(new SessionHelper()).As<ICacheHelper>();
 
-            ICacheHelper cacheHelper = null;
-            try
-            {
-                StackExchangeRedisCacheClient stackExchangeRedisCacheClient = new StackExchangeRedisCacheClient(new JilSerializer());
-                stackExchangeRedisCacheClient.Dispose();
-                cacheHelper = new RedisHelper();
-            }
-            catch (TypeInitializationException)
-            {
-                cacheHelper = new SessionHelper();
-            }
-            finally
-            {
-                Builder.RegisterInstance(cacheHelper).As<ICacheHelper>();
-            }
+            //ICacheHelper cacheHelper = null;
+            //try
+            //{
+            //    StackExchangeRedisCacheClient stackExchangeRedisCacheClient = new StackExchangeRedisCacheClient(new JilSerializer());
+            //    stackExchangeRedisCacheClient.Dispose();
+            //    cacheHelper = new RedisHelper();
+            //}
+            //catch (TypeInitializationException)
+            //{
+            //    cacheHelper = new SessionHelper();
+            //}
+            //finally
+            //{
+            //    Builder.RegisterInstance(cacheHelper).As<ICacheHelper>();
+            //}
         }
 
         private static void BuildContainer()
