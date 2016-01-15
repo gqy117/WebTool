ed42c9acc302aa37e4e12f912003eb95150fa451
Leo Li <gqy117@gmail.com>
2016-01-15 18:51:00 +1300

    [master] Changed website port.

--- CreateIISWebSite.ps1
+++ CreateIISWebSite.ps1
@@ -1,5 +1,5 @@
 New-WebAppPool WebTool
 Set-ItemProperty IIS:\AppPools\WebTool managedRuntimeVersion v4.0
 
-New-Item iis:\Sites\WebTool -bindings @{protocol="http";bindingInformation=":81:WebTool"} -physicalPath "$($env:appveyor_build_folder)\WebSite\WebTool"
+New-Item iis:\Sites\WebTool -bindings @{protocol="http";bindingInformation=":12598:WebTool"} -physicalPath "$($env:appveyor_build_folder)\WebSite\WebTool"
 Set-ItemProperty IIS:\Sites\WebTool -name applicationPool -value WebTool
