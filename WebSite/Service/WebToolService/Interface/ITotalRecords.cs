cf9ac20f96eecc77f7018704f725247fec07a5b5
Leo Li <gqy117@gmail.com>
2016-01-23 07:49:51 +1300

    [CleanUp] Moved "WebToolServiceBase" to "WebToolSerivce".
    Former-commit-id: 310436ecdd4a54e23b77a2b11cf4211d193638f8 [formerly 310436ecdd4a54e23b77a2b11cf4211d193638f8 [formerly 310436ecdd4a54e23b77a2b11cf4211d193638f8 [formerly 310436ecdd4a54e23b77a2b11cf4211d193638f8 [formerly 38e41b20272803a5371e70ccb58f561b8e87a06b]]]]
    Former-commit-id: 9dae01f2eb91c9e2782f2dc590665c607b213405
    Former-commit-id: e049132cd1c65458dd97dfb12d0a750b50117187
    Former-commit-id: 5e77a11523d6f23c06464e9cbb03dc009b55958a
    Former-commit-id: 0f27d2ebdad668f05b9a9153a1cfa4bc37641ea5

---
+++ WebSite/Service/WebToolService/Interface/ITotalRecords.cs
@@ -0,0 +1,12 @@
+﻿namespace WebToolService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface ITotalRecords
+    {
+        int TotalRecords { get; set; }
+    }
+}
