f1f709ee3878db9521e5a5566566e965db84dad1
Leo Li <gqy117@gmail.com>
2016-01-23 14:32:31 +1300

    [CleanUp] Removed Memcached. Removed Capcha. Moved DI Code to DependencyInjection.csproj.
    Former-commit-id: 5c6b87402bde62b41682d51bf08b9fed2627723e [formerly 5c6b87402bde62b41682d51bf08b9fed2627723e [formerly 5c6b87402bde62b41682d51bf08b9fed2627723e [formerly 5c6b87402bde62b41682d51bf08b9fed2627723e [formerly d6b2b28400c500f2f1f74af03002440473c41134]]]]
    Former-commit-id: 6d5bd20a3da14cdab5b426ca0f58bc213efd6186
    Former-commit-id: 7951196b51dbdf1d2f2c923fffe7fcd6ccede2da
    Former-commit-id: d6b92ae741f96f2c6ab004a12c6c899a492e496b
    Former-commit-id: ab09df7fa28b706b83da684853646160a027bb93

--- WebSite/WebTool/Controllers/Account/AccountController.cs
+++ WebSite/WebTool/Controllers/Account/AccountController.cs
@@ -8,7 +8,6 @@ namespace WebTool
     using System.Web.Routing;
     using System.Web.Security;
     using System.Web.SessionState;
-    using CaptchaMvc.Attributes;
     using WebMatrix.WebData;
     using WebToolCulture.Resource;
     using WebToolService;
