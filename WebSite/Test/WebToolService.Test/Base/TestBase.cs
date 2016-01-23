47332cad8da214d52b8b05d9fefe8e5c05fa8753
Leo Li <gqy117@gmail.com>
2016-01-23 15:52:50 +1300

    [CleanUp] Added FluentAssertions.
    Former-commit-id: a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly 066701d4edab1a0cec32332f88da1ff58ef39fa1]]]]
    Former-commit-id: 75590a02e4670eaf6b5fdd350efed91454d00d1e
    Former-commit-id: 85edc18960bf2fe16cb25b16d3303aed5d332dd0
    Former-commit-id: a52836ed1e792916ca7fa93407fe94142ed1f83c
    Former-commit-id: 51980bb72c38a032a59f45649b8396ef8788bb71

---
+++ WebSite/Test/WebToolService.Test/Base/TestBase.cs
@@ -0,0 +1,25 @@
+﻿namespace WebToolService.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Practices.Unity;
+    using NUnit.Framework;
+    using WebTool;
+
+    public abstract class TestBase
+    {
+        protected IUnityContainer Container { get; set; }
+
+        [TestFixtureSetUp]
+        public virtual void Init()
+        {
+            BootStrap bootStrap = new BootStrap();
+            bootStrap.Configure();
+
+            this.Container = bootStrap.MyContainer;
+        }
+    }
+}
