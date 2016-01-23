0ba7faf9c05a69cb091e0dab7d6dcfcd69f9b8c8
Leo Li <gqy117@gmail.com>
2016-01-23 15:52:50 +1300

    [CleanUp] Added FluentAssertions.
    Former-commit-id: a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly a0471c0235e063f6a31c8a8a0a6304e5e284f015 [formerly 911d9e9cb4bc5c841c3979abc7eb983e2d61d40a]]]]
    Former-commit-id: 75590a02e4670eaf6b5fdd350efed91454d00d1e
    Former-commit-id: 85edc18960bf2fe16cb25b16d3303aed5d332dd0
    Former-commit-id: a52836ed1e792916ca7fa93407fe94142ed1f83c
    Former-commit-id: 51980bb72c38a032a59f45649b8396ef8788bb71

--- WebSite/Test/WebToolService.Test/Service/Account/UserServiceTest.cs
+++ WebSite/Test/WebToolService.Test/Service/Account/UserServiceTest.cs
@@ -5,15 +5,39 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using FluentAssertions;
+    using Microsoft.Practices.Unity;
     using NUnit.Framework;
+    using WebTool;
 
     [TestFixture]
-    public class UserServiceTest
+    public class UserServiceTest : TestBase
     {
+        private UserService UserService { get; set; }
+
+        [TestFixtureSetUp]
+        public override void Init()
+        {
+            base.Init();
+            this.UserService = this.Container.Resolve<UserService>();
+        }
+
         [Test]
         public void GetUserModelByName_ShouldReturn1_WhenTheUserNameIs1()
         {
-            
+            // Arrange
+
+            // Act
+            var actual = this.UserService.GetUserModelByName("1");
+
+            // Assert
+            UserModel expected = new UserModel()
+            {
+                UserId = 1,
+                UserName = "1",
+            };
+
+            actual.ShouldBeEquivalentTo(expected);
         }
     }
 }
