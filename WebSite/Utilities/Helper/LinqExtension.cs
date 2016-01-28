7ce111a091bad98b8e739369d0e1cad07cc1b7c8
Leo Li <gqy117@gmail.com>
2016-01-28 20:02:43 +1300

    [CleanUp] Added the "Search" method as a linq extension method.

---
+++ WebSite/Utilities/Helper/LinqExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace System.Linq
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Expressions;
+
+    public static class LinqExtension
+    {
+        public static IQueryable<TSource> Search<TSource>(this IQueryable<TSource> source, string search, Expression<Func<TSource, bool>> predicate)
+        {
+            if (String.IsNullOrEmpty(search))
+            {
+                return source;
+            }
+            else
+            {
+                return source.Where(predicate);
+            }
+        }
+    }
+}
