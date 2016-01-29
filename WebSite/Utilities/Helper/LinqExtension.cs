c8f10bde106e22ef10c52867d374143a3f262b03
Leo Li <gqy117@gmail.com>
2016-01-30 08:10:09 +1300

    [CleanUp] Implemented OrderBy.
    Former-commit-id: fee8756d0fcd35069476ddadf9724284538f7196 [formerly fee8756d0fcd35069476ddadf9724284538f7196 [formerly fee8756d0fcd35069476ddadf9724284538f7196 [formerly fee8756d0fcd35069476ddadf9724284538f7196 [formerly a4dac9aa9655f461ca5d40153d9bbd8a5041ebed]]]]
    Former-commit-id: bec14599c5c5104d2f04b2d572900a444199fbc5
    Former-commit-id: 3f423b40af1da103ade09c22b0900d2d22b50c37
    Former-commit-id: 585caeb49f23b58eef378ced2352a2278c18fd89
    Former-commit-id: 0254b2cbbebe3ae4fc0b1ccb2caf102268be17c2

--- WebSite/Utilities/Helper/LinqExtension.cs
+++ WebSite/Utilities/Helper/LinqExtension.cs
@@ -20,5 +20,21 @@
                 return source.Where(predicate);
             }
         }
+
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string ordering)
+        {
+            string[] sortArray = ordering.Split(' ');
+            string sortCol = sortArray[0];
+            string sortDir = sortArray[1];
+
+            var type = typeof(T);
+            var property = type.GetProperty(sortCol);
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExp = Expression.Lambda(propertyAccess, parameter);
+            MethodCallExpression resultExp = Expression.Call(typeof(Queryable), sortDir, new Type[] { type, property.PropertyType }, source.Expression, Expression.Quote(orderByExp));
+            
+            return source.Provider.CreateQuery<T>(resultExp);
+        }
     }
 }
