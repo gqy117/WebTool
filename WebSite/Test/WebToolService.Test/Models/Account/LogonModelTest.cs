b735ba40e4479a947aa7d3db4fa111b57e84a992
Leo Li <gqy117@gmail.com>
2016-01-30 18:40:43 +1300

    [CleanUp] Added test cases for RequiredExt.cs.
    Former-commit-id: bb33da1a9d63647e84328024b45a1c130a3cbef1 [formerly bb33da1a9d63647e84328024b45a1c130a3cbef1 [formerly bb33da1a9d63647e84328024b45a1c130a3cbef1 [formerly bb33da1a9d63647e84328024b45a1c130a3cbef1 [formerly 03928b368311948cb52ee1d6c6e1b86e6e414d64]]]]
    Former-commit-id: 335655a574b2587c1df51710012f03036a8f9048
    Former-commit-id: e900db87aaad004293444064f0ef8701592c483c
    Former-commit-id: ee16bfcbafecf7dfb28e0f167e967a25d315a2e3
    Former-commit-id: 71d9759d0e19e4fb330af3fd76d8c905764734c4

---
+++ WebSite/Test/WebToolService.Test/Models/Account/LogonModelTest.cs
@@ -0,0 +1,28 @@
+﻿namespace WebToolService.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LogonModelTest
+    {
+        [Test]
+        public void RememberMe_ShouldBeTrue_ByDefault()
+        {
+            // Arrange
+            LogOnModel logOnModel = new LogOnModel();
+
+            // Action
+            bool actual = logOnModel.RememberMe;
+
+            // Assert
+            bool expected = true;
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
