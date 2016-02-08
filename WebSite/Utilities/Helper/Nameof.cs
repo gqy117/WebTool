dedc1ff6acf17f619bf8acc0ed3c08f1dbbf12ad
Leo Li <gqy117@gmail.com>
2016-02-08 15:03:47 +1300

    [ServiceLayerInterface] Fixed the compilation error.

--- WebSite/Utilities/Helper/Nameof.cs
+++ WebSite/Utilities/Helper/Nameof.cs
@@ -7,14 +7,19 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1053:StaticHolderTypesShouldNotHaveConstructors", Justification = "1")]
     public class Nameof<T>
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1011:ConsiderPassingBaseTypesAsParameters", Justification = "1"), 
+        System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1000:DoNotDeclareStaticMembersOnGenericTypes", Justification = "1")]
         public static string Property<TProp>(Expression<Func<T, TProp>> expression)
         {
             var body = expression.Body as MemberExpression;
 
             if (body == null)
+            {
                 throw new ArgumentException("'expression' should be a member expression");
+            }
 
             return body.Member.Name;
         }
