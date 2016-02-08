70e34f40a23279fed894986f3ef390912d5fc6ef
Leo Li <gqy117@gmail.com>
2016-02-08 14:46:07 +1300

    [ServiceLayerInterface] Added Nameof.cs and its tests.

---
+++ WebSite/Utilities/Helper/Nameof.cs
@@ -0,0 +1,22 @@
+﻿namespace Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Nameof<T>
+    {
+        public static string Property<TProp>(Expression<Func<T, TProp>> expression)
+        {
+            var body = expression.Body as MemberExpression;
+
+            if (body == null)
+                throw new ArgumentException("'expression' should be a member expression");
+
+            return body.Member.Name;
+        }
+    }
+}
