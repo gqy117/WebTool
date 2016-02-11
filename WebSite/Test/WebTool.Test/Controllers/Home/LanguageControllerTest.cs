7b335c301a3f83a7e6f2938bf002d303ada0c7f4
Leo Li <gqy117@gmail.com>
2016-02-11 18:41:13 +1300

    [ServiceLayerInterface] Added test cases for LanguageController.cs.

---
+++ WebSite/Test/WebTool.Test/Controllers/Home/LanguageControllerTest.cs
@@ -0,0 +1,48 @@
+﻿namespace WebTool.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using FluentAssertions;
+    using Microsoft.Practices.Unity;
+    using Moq;
+    using NUnit.Framework;
+    using WebToolService;
+
+    [TestFixture]
+    public class LanguageControllerTest : ControllerTestBase
+    {
+        protected override Controller Controller { get { return this.LanguageController; } }
+
+        private LanguageController LanguageController { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            base.Init();
+        }
+
+        protected override void InitController()
+        {
+            this.LanguageController = this.Container.Resolve<LanguageController>();
+        }
+
+        [Test]
+        public void Index_ShouldReturnPartialViewWithTheNameOfLanguageBar_AndCallGetLanguageModelMethod()
+        {
+            // Arrange
+            this.MockLanguageService.Setup(x => x.GetLanguageModel(It.IsAny<string>())).Returns(null as LanguageModel);
+
+            // Act
+            var result = this.LanguageController.Index();
+            string actual = (result as PartialViewResult).ViewName;
+
+            // Assert
+            string expected = "~/Views/Head/Language/LanguageBar.cshtml";
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
