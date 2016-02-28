ee7c91cd2b39967c897688e7412f1214d7115ed7
Leo Li <Leo.Li@myob.com>
2016-02-29 11:25:35 +1300

    Refined the Setup.script.
    Former-commit-id: 603258f485c3b13a3da2dfcfe71c31d1066ec865 [formerly 68b69f95ca3fd774f7b66c888b3de0c398073994]
    Former-commit-id: f0fd3f9f0f33e23974484390145170e2cc8aa777
    Former-commit-id: 2f6d5ba7f7cc116b441039583e1878048d19f018
    Former-commit-id: 6a26d9a1536073de2557b832fa14605a95476e91
    Former-commit-id: 4e7b600c2fc0548fd87515d97fe89a2353e65dab

--- Setup.ps1
+++ Setup.ps1
@@ -1,10 +1,14 @@
 npm install -g gulp
-npm install --prefix WebSite
 npm install -g karma
 npm install -g karma-jasmine karma-chrome-launcher karma-jasmine-html-reporter karma-spec-reporter
 npm install -g phantomjs
 npm install -g bower
 npm install -g bower-installer
+
+npm install --prefix WebSite
+#cd WebSite
+#bower-installer
+#cd ..
 
 copy WebSite\WebTool\connectionStrings.template.config WebSite\WebTool\connectionStrings.config
 copy WebSite\DataAccess\connectionStrings.template.config WebSite\DataAccess\connectionStrings.config
