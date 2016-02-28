490b2fd335fc57bcb9e9e16d977d7740ea3d9d86
Leo Li <Leo.Li@myob.com>
2016-02-29 09:38:45 +1300

    Install bower and bower-installer.
    Former-commit-id: a49e578c78151b7164a095c097b109d4c83ef857 [formerly dfaeb8f8179d918ba572174baa010c42b5fdd092]
    Former-commit-id: 647bca000ce2a7b196dcef101f5b4f491542cae7
    Former-commit-id: 160e345b06e054c369193e3f4fb5008318537d61
    Former-commit-id: fcfc9f18de0c4418553a2d9764befc7d6e497918
    Former-commit-id: 6effb2d236c60be25e5b306cee518a880f9b82aa

--- Setup.ps1
+++ Setup.ps1
@@ -3,6 +3,8 @@ npm install --prefix WebSite
 npm install -g karma
 npm install -g karma-jasmine karma-chrome-launcher karma-jasmine-html-reporter karma-spec-reporter
 npm install -g phantomjs
+npm install -g bower
+npm install -g bower-installer
 
 copy WebSite\WebTool\connectionStrings.template.config WebSite\WebTool\connectionStrings.config
 copy WebSite\DataAccess\connectionStrings.template.config WebSite\DataAccess\connectionStrings.config
