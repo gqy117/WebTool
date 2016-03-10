9b8f9054890046141f5e72402b67f7ca37b0ebd3
Leo Li <gqy117@gmail.com>
2016-03-10 19:21:16 +1300

    Added JsonTableAttribute.cs and its test cases.

---
+++ WebSite/Test/WebTool.Test/Filter/JsonTableFilterTest.cs
@@ -0,0 +1,84 @@
+﻿namespace WebTool.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Moq;
+    using NUnit.Framework;
+    using WebToolService;
+
+    [TestFixture]
+    public class JsonTableFilterTest
+    {
+        private ActionExecutingContext ControllerContext;
+
+        private Mock<TableBaseController> MockTableBaseController;
+
+        private Expression<Action<TableBaseController>> ReBindJQueryTable;
+
+        private Expression<Func<TableBaseController, ActionResult>> JsonTable;
+
+        private JsonTableAttribute JsonTableAttribute { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            this.JsonTableAttribute = new JsonTableAttribute();
+
+            this.MockTableBaseController = new Mock<TableBaseController>();
+
+            this.InitControllerContext();
+            this.InitMockMethods();
+        }
+
+        [Test]
+        public void OnActionExecuting_ShouldReturn_WhenTheControllerIsNotTableBaseController()
+        {
+            // Arrange
+            this.ControllerContext.Controller = new HomeController();
+
+            // Act
+            this.JsonTableAttribute.OnActionExecuting(ControllerContext);
+
+            // Assert
+            MockTableBaseController.Verify(ReBindJQueryTable, Times.Never);
+            MockTableBaseController.Verify(JsonTable, Times.Never);
+        }
+
+        [Test]
+        public void OnActionExecuting_ShouldCallReBindJQueryTableAndJsonTable_WhenTheControllerIsTableBaseController()
+        {
+            // Arrange
+
+            // Act
+            this.JsonTableAttribute.OnActionExecuting(ControllerContext);
+
+            // Assert
+            MockTableBaseController.Verify(ReBindJQueryTable, Times.Once);
+            MockTableBaseController.Verify(JsonTable, Times.Once);
+        }
+
+        private void InitControllerContext()
+        {
+            this.ControllerContext = new ActionExecutingContext();
+            this.ControllerContext.Controller = this.MockTableBaseController.Object;
+            this.ControllerContext.ActionParameters = new Dictionary<string, object>()
+            {
+                { "model", new JQueryTable() }
+            };
+        }
+
+        private void InitMockMethods()
+        {
+            this.ReBindJQueryTable = x => x.ReBindJQueryTable(It.IsAny<JQueryTable>());
+            this.JsonTable = x => x.JsonTable(It.IsAny<JQueryTable>());
+
+            this.MockTableBaseController.Setup(ReBindJQueryTable);
+            this.MockTableBaseController.Setup(JsonTable);
+        }
+    }
+}
---
+++ WebSite/WebTool/Filter/JsonTableAttribute.cs
@@ -0,0 +1,36 @@
+﻿namespace WebTool
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using WebToolService;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class JsonTableAttribute : BaseAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var tableBaseController = filterContext.Controller as TableBaseController;
+            JQueryTable model = filterContext.ActionParameters["model"] as JQueryTable;
+
+            if (tableBaseController == null
+                || model == null)
+            {
+                return;
+            }
+
+            tableBaseController.ReBindJQueryTable(model);
+
+            base.OnActionExecuting(filterContext);
+
+            this.SetJsonTableResult(filterContext, tableBaseController, model);
+        }
+
+        private void SetJsonTableResult(ActionExecutingContext filterContext, TableBaseController tableBaseController, JQueryTable model)
+        {
+            filterContext.Result = tableBaseController.JsonTable(model);
+        }
+    }
+}
