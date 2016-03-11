dcc146b8a43bfb94ec2b075de3182d6e7edb43ea
Leo Li <gqy117@gmail.com>
2016-03-12 12:03:39 +1300

    [ServiceStackRedis] Added Id to User.cs.

--- WebSite/DataAccess/User.cs
+++ WebSite/DataAccess/User.cs
@@ -14,18 +14,9 @@ namespace WebToolRepository
     
     public partial class User
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public User()
-        {
-            this.WOLs = new HashSet<WOL>();
-        }
-    
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public Nullable<System.DateTime> CreationDate { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<WOL> WOLs { get; set; }
     }
 }
