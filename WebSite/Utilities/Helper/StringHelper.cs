f74fa37444068ef0b82368842c1f02fac1114bb4
Leo Li <gqy117@gmail.com>
2016-03-12 15:30:15 +1300

    [ServiceStackRedis] Added StringHelper.cs and its test cases.

---
+++ WebSite/Utilities/Helper/StringHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Utilities
+{
+    using System;
+
+    public static class StringHelper
+    {
+        public static bool Contains(this string source, string toCheck)
+        {
+            return source.IndexOf(toCheck, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+    }
+}
