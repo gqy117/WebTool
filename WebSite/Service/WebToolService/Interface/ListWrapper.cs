9656a9fbc1b413c7c6150b3461cc10c1f747b64c
Leo Li <gqy117@gmail.com>
2016-03-19 12:18:01 +1300

    [ListWrapper] Added ListWrapper.cs.

---
+++ WebSite/Service/WebToolService/Interface/ListWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace WebToolService
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ListWrapper<T> : ITotalRecords
+    {
+        public IList<T> List { get; set; }
+
+        public int TotalRecords { get; set; }
+    }
+}
