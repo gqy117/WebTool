3fb3cd74e1c0ae41d8762a62e061bc525bb3faf4
Leo Li <gqy117@gmail.com>
2016-05-14 09:25:32 +1200

    [master] Bundled head.js.

--- WebSite/WebTool/App_Start/Bundle/Shared/Head.cs
+++ WebSite/WebTool/App_Start/Bundle/Shared/Head.cs
@@ -6,13 +6,7 @@
     {
         public static readonly IEnumerable<string> Files = new List<string>
         {
-            "~/Views/Sidebar/activePanel.module.js",
-            "~/Views/Sidebar/sidebar.service.js",
-            "~/Views/Sidebar/sidebar.controller.js",
-            "~/Views/Head/head.service.js",
-            "~/Views/Head/head.controller.js",
-            "~/Views/Head/Language/languageBar.service.js",
-            "~/Views/Head/Language/languageBar.controller.js"
+            "~/Content/head.js"
         };
     }
 }
