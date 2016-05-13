6baa02ca9215780e012f2a71a676f8411fd005d0
Leo Li <gqy117@gmail.com>
2016-05-14 09:21:55 +1200

    [master] Bundled main.min.js.

--- WebSite/WebTool/App_Start/Bundle/Shared/MainJs.cs
+++ WebSite/WebTool/App_Start/Bundle/Shared/MainJs.cs
@@ -6,20 +6,7 @@
     {
         public static readonly IEnumerable<string> Files = new List<string>
         {
-            "~/Scripts/jquery/jquery-1.8.3.min.js",
-            "~/Scripts/angular/angular.js",
-            "~/Content/assets/uniform/jquery.uniform.min.js",
-            "~/Content/assets/breakpoints/breakpoints.js",
-            "~/Content/assets/bootstrap/js/bootstrap.min.js",
-            "~/Scripts/jquery.cookie.js",
-            "~/Content/assets/plugins/bootstrap-modal/js/bootstrap-modal.js",
-            "~/Content/assets/plugins/bootstrap-modal/js/bootstrap-modalmanager.js",
-            "~/Views/Shared/Library/main.app.js",
-            "~/Content/assets/js/app.js",
-            "~/Views/Shared/Library/ga.service.js",
-            "~/Views/Shared/Library/jQuery.service.js",
-            "~/Views/Shared/Init/uiModel.service.js",
-            "~/Views/Shared/Init/main.controller.js"
+            "~/Content/main.js"
         };
     }
 }
