7b3687f810d486deb3e265c406842130bc5729eb
Leo Li <gqy117@gmail.com>
2016-08-19 17:22:23 +1200

    [master] Removed generated js files.

--- WebSite/WebTool/Views/Shared/Library/jQuery.service.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    "use strict";
-    angular.module("mainApp")
-        .factory("jQuery", jQueryService);
-    jQueryService.$inject = ["$window"];
-    function jQueryService($window) {
-        return $window.jQuery;
-    }
-}());
